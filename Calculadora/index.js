3cd47a0e74e485fc082052048237a12f573e2ed6
lucasespancaveia <138123599+lucasespancaveia@users.noreply.github.com>
2023-09-27 10:59:48 -0300

    Add files via upload

---
+++ Calculadora/index.js
@@ -0,0 +1,23 @@
+btn.addEventListener('click', function(){
+
+    var a = valorA.value;
+    var b = valorB.value;
+var op = operacao.value;
+
+if(op == 1){
+    resultado.innerHTML="Resultado: " + (a+b)
+}
+
+if(op == 2){
+    resultado.innerHTML="Resultado: " + (a-b)
+}
+
+if(op == 3){
+    resultado.innerHTML="Resultado: " + (a*b)
+}
+
+if(op == 4){
+    resultado.innerHTML="Resultado: " + (a/b)
+}
+
+});
